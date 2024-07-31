2125f9837bb92495c1e6db9076e6a1d15085a965
itsquelc <raquelnunescirino@gmail.com>
2024-07-31 14:49:05 -0300

    CONTINUAÇAO

--- QuelloFlix/Program.cs
+++ QuelloFlix/Program.cs
@@ -1,15 +1,25 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using QuelloFlix.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Serviço de conexão com o banco de dados
 string conn = builder.Configuration. 
 GetConnectionString("QuelloFlixConnection");
 var version = ServerVersion.AutoDetect(conn);
 builder.Services.AddDbContext<AppDbContext>(
     opt => opt.UseMySql(conn, version)
 );
+
+
+// Serviço de gestão de usúarios
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(
+    opt => opt.SignIn.RequireConfirmedAccount = false
+)
+.AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
 
 builder.Services.AddControllersWithViews();
 
